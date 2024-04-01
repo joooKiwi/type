268cd247d322acb7666b99c22b3d907421f5b9e6
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-03-31 20:13:48 -0400

    Separation of the numeric types into different files

---
+++ typescript/src/numeric (number).ts
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ Copyright (c) 2023-2024. Jonathan Bédard ~ JóôòKiwi
+
+ This project is free to use.
+ All the right is reserved to the author of this project.
+ ******************************************************************************/
+
+/**
+ * A type-alias for a {@link Number}
+ * in a {@link String} template
+ *
+ * @see Template
+ * @see NumericTemplate
+ */
+export type NumberTemplate<T extends number = number, > = `${T}`
+
+/**
+ * A type-alias for a {@link Number}
+ * as a primitive or in a {@link String} template
+ *
+ * @see Template
+ * @see TemplateOrNumeric
+ * @see TemplateOrNumberOrObject
+ */
+export type TemplateOrNumber<T extends number = number, > = | T | `${T}`
+
+/**
+ * A type-alias for a {@link Number}
+ * as a primitive or an object
+ *
+ * @see NumericOrObject
+ * @see TemplateOrNumberOrObject
+ */
+export type NumberOrObject<T extends number = number, > = | T | Number
+
+/**
+ * A type-alias for a {@link Number}
+ * as a primitive, an object or in a {@link String} template
+ *
+ * @see Template
+ * @see TemplateOrNumber
+ * @see NumberOrObject
+ * @see TemplateOrNumericOrObject
+ */
+export type TemplateOrNumberOrObject<T extends number = number, > = | T | Number | `${T}`
--- typescript/src/numeric.ts
+++ typescript/src/numeric.ts
@@ -5,137 +5,51 @@
  All the right is reserved to the author of this project.
  ******************************************************************************/
 
-//#region -------------------- number --------------------
-
-/**
- * A type-alias for a {@link Number}
- * in a {@link String} template
- *
- * @see NumericTemplate
- */
-export type NumberTemplate<T extends number = number, > = `${T}`
-/**
- * A type-alias for a {@link Number}
- * as a primitive or in a {@link String} template
- *
- * @see PossibleNumberOrTemplate
- * @see NumericOrTemplate
- */
-export type NumberOrTemplate<T extends number = number, > = | T | `${T}`
-
-/**
- * A type-alias for a {@link Number}
- * as a primitive or an object
- *
- * @see PossibleNumeric
- */
-export type PossibleNumber<T extends number = number, > = | T | Number
-/**
- * A type-alias for a {@link Number}
- * as a primitive, an object or in a {@link String} template
- *
- * @see NumberOrTemplate
- * @see PossibleNumericOrTemplate
- */
-export type PossibleNumberOrTemplate<T extends number = number, > = | T | Number | `${T}`
-
-//#endregion -------------------- number --------------------
-//#region -------------------- bigint --------------------
-
-/** A type-alias for a {@link BigInt}
- * in a {@link String} template
- *
- * @see NumericTemplate
- */
-export type BigIntTemplate<T extends bigint = bigint, > = `${T}`
-/**
- * A type-alias for a {@link Number}
- * as a primitive or in a {@link String} template
- *
- * @see PossibleBigIntOrTemplate
- * @see NumericOrTemplate
- */
-export type BigIntOrTemplate<T extends bigint = bigint, > = | T | `${T}`
-
-/**
- * A type-alias for a {@link BigInt}
- * as a primitive or an object
- *
- * @see PossibleNumeric
- */
-export type PossibleBigInt<T extends bigint = bigint, > = | T | BigInt
-/**
- * A type-alias for a {@link BigInt}
- * as a primitive, an object or in a {@link String} template
- *
- * @see BigIntOrTemplate
- * @see PossibleNumericOrTemplate
- */
-export type PossibleBigIntOrTemplate<T extends bigint = bigint, > = | T | BigInt | `${T}`
-
-//#endregion -------------------- bigint --------------------
-//#region -------------------- numeric --------------------
-
 /** A type-alias for a {@link Number} or a {@link BigInt} as a primitive */
 export type Numeric<T extends | number | bigint = | number | bigint, > = T
+
 /**
  * A type-alias for a value ({@link Number} or {@link BigInt})
  * in a {@link String} template
  *
  * @see Numeric
+ * @see Template
  * @see NumberTemplate
  * @see BigIntTemplate
  */
 export type NumericTemplate<T extends Numeric = Numeric, > = `${T}`
+
 /**
  * A type-alias for a value ({@link Number} or {@link BigInt})
  * as a primitive or in a {@link String} template
  *
  * @see Numeric
- * @see NumberOrTemplate
- * @see BigIntOrTemplate
- * @see PossibleNumericOrTemplate
+ * @see Template
+ * @see TemplateOrNumber
+ * @see TemplateOrBigInt
+ * @see TemplateOrNumericOrObject
  */
-export type NumericOrTemplate<T extends Numeric = Numeric, > = | T | `${T}`
+export type TemplateOrNumeric<T extends Numeric = Numeric, > = | T | `${T}`
 
 /**
  * A type-alias for a value ({@link Number} or {@link BigInt})
  * as a primitive or an object
  *
  * @see Numeric
- * @see PossibleNumber
- * @see PossibleBigInt
+ * @see NumberOrObject
+ * @see BigIntOrObject
+ * @see TemplateOrNumericOrObject
  */
-export type PossibleNumeric<T extends Numeric = Numeric, > = | T | Numeric
+export type NumericOrObject<T extends Numeric = Numeric, > = | T | Number | BigInt
+
 /**
  * A type-alias for a value ({@link Number} or {@link BigInt})
  * as a primitive, an object or in a {@link String} template
  *
  * @see Numeric
- * @see PossibleNumberOrTemplate
- * @see PossibleBigIntOrTemplate
- * @see NumericOrTemplate
+ * @see Template
+ * @see TemplateOrNumberOrObject
+ * @see TemplateOrBigIntOrObject
+ * @see TemplateOrNumeric
  */
-export type PossibleNumericOrTemplate<T extends Numeric = Numeric, > = | T | Numeric | `${T}`
-
-//#endregion -------------------- numeric --------------------
-//#region -------------------- string --------------------
-
-/**
- * A type-alias for a value ({@link String}, {@link Number} or {@link BigInt})
- * as a primitive
- *
- * @see Numeric
- * @see PossibleStringOrNumeric
- */
-export type StringOrNumeric<T extends | string | number | bigint = | string | number | bigint, > = T
-/**
- * A type-alias for a value ({@link String}, {@link Number} or {@link BigInt})
- * as a primitive or an object
- *
- * @see StringOrNumeric
- */
-export type PossibleStringOrNumeric<T extends StringOrNumeric = StringOrNumeric, > = | T | String | Number | BigInt
-
-//#endregion -------------------- string --------------------
-
+export type TemplateOrNumericOrObject<T extends Numeric = Numeric, > = | T | Number | BigInt | `${T}`
