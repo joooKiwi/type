09747c17e0dffe34882c16a84dff759e714e520b
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-03-31 20:16:08 -0400

    Addition of other characters

--- typescript/src/character.ts
+++ typescript/src/character.ts
@@ -7,9 +7,15 @@
 
 /** A type-alias for the space character */
 export type Space = ' '
-/** A type-alias for the comma character */
+/** A type-alias for the "tabulation" (\t) {@link String character} */
+export type Tabulation = '\t'
+/** A type-alias for the "enter" (\n) {@link String character} */
+export type Enter = '\n'
+/** A type-alias for the "comma" {@link String character} */
 export type Comma = ','
-/** A type-alias for the infinity character */
-export type Infinity = '∞'
-/** A type-alias for the suspension point character */
+/** A type-alias for the "point" {@link String character} */
+export type Point = '.'
+/** A type-alias for the "infinity" {@link String character} */
+export type InfinityString = '∞'
+/** A type-alias for the "suspension point" {@link String character} */
 export type SuspensionPoint = '…'
